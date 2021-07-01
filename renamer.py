e4ca7a0d4f7857b6489f9ee966b88cd41860d66b
Pieter Zeilstra <kopop8@live.nl>
2021-07-01 16:41:35 +0200

    Fixed code

--- renamer.py
+++ renamer.py
@@ -3,11 +3,11 @@ import csv
 import os.path
 from os import path
 def renameDirectories(sort):
-    r = csv.reader(open(sort+'_labeled_studies.csv')) # Here your csv file
+    r = csv.reader(open(sort+'_labeled_studies.csv'))
     lines = list(r)
     for (line, label) in lines:
-        if not path.exists(line):
-            dest = line.replace('MURA-v1.1/','')
+        dest = line.replace('MURA-v1.1/','')
+        if not path.exists(dest):
             source = dest.replace('positive','negative')
             os.rename(source,dest)
 renameDirectories('valid')
